954ad105254e037952f3283f06fb1b813597d52b
jplatter <Jason@DadsLappy>
2016-03-04 18:54:46 -0800

    Player class creation 1-29-2016

---
+++ StarWars/src/byui/cit260/starwars/controller/LightsaberController.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package byui.cit260.starwars.controller;
+
+/**
+ *
+ * @author Jason
+ */
+public class LightsaberController {
+    public double coneVolume(double radius, double height) {
+        
+        if (radius <= 0 || height <= 0) {
+            return -1;
+        }
+        double volume = Math.Pi * radius *radius * (height/3);
+        
+        return volume;
+}
